1634297030061bb458fd64eea4eba8d9cd7ce834
Sebastian Wehner <Basti.wehner@googlemail.com>
2020-01-13 19:21:32 +0100

    added initial control panel

--- control-panel-launcher.js
+++ control-panel-launcher.js
@@ -1,45 +1,49 @@
-var landingPage = "/control-panel/index.html";
+(function(){
 
-var browser;
-if (typeof InstallTrigger !== 'undefined'){ // firefox
-  browser = browser;
-} else if (!!window.chrome && (!!window.chrome.webstore || !!window.chrome.runtime)) { // chrome
-  browser = chrome;
-}
+  const landingPage = "/control-panel/index.html";
+
+  /*var browser;
+  if (typeof InstallTrigger !== 'undefined'){ // firefox
+    browser = browser;
+  } else if (!!window.chrome && (!!window.chrome.webstore || !!window.chrome.runtime)) { // chrome
+    browser = chrome;
+  }*/
 
 
-/************      utiilitiy functions      ************/
-function focusOrOpenLandigpageInTab(url){
-  browser.tabs.query({}, (tabs)=>{
-    let isOpen = false;
-    for (let i=tabs.length-1; i>=0; i--){
-      if (tabs[i].url.includes(url)) {
-        // the url is already open in this tab
-        isOpen = true;
-        browser.tabs.update(tabs[i].id, {active: true});
-        break;
+  /************      utiilitiy functions      ************/
+  function focusOrOpenLandigpageInTab(url){
+    browser.tabs.query({}, (tabs)=>{
+      let isOpen = false;
+      for (let i=tabs.length-1; i>=0; i--){
+        if (tabs[i].url.includes(url)) {
+          // the url is already open in this tab
+          isOpen = true;
+          browser.tabs.update(tabs[i].id, {active: true});
+          break;
+        }
       }
-    }
-    if (!isOpen){ // the url was not yet open
-        browser.tabs.create({url: url});
-        return true;
-    }
-  });
-  return false;
-}
+      if (!isOpen){ // the url was not yet open
+          browser.tabs.create({url: url});
+          return true;
+      }
+    });
+    return false;
+  }
 
 
-/************      setup functions      ************/
+  /************      setup functions      ************/
 
-// create a new tab and open the landing page
-// for the interaction with the application
-function setupLandingPage(event){
-  return focusOrOpenLandigpageInTab(landingPage);
-}
+  // create a new tab and open the landing page
+  // for the interaction with the application
+  function setupLandingPage(event){
+    return focusOrOpenLandigpageInTab(landingPage);
+  }
 
 
-// main starting point
-// from here on a tab with the landing page should be created
-// an initial database connection established before the
-// landing page is displayed
-browser.browserAction.onClicked.addListener(setupLandingPage);
+  // main starting point
+  // from here on a tab with the landing page should be created
+  // an initial database connection established before the
+  // landing page is displayed
+  browser.browserAction.onClicked.addListener(setupLandingPage);
+
+}) ();
--- control-panel/control-panel.js
+++ control-panel/control-panel.js
@@ -1,13 +1,65 @@
 
+const allowedSymbols = ["MIOTA"];
+const allowedInputKeys = [48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 188, 190];
 
-document.addEventListener("DomContentLoaded", ()=>{
+const calcButton = document.querySelector("#calc-button");
+const fiatInput = document.querySelector("#fiat-userinput");
+const cryptOutput = document.querySelector("#crypto-userinput");
+
+var currentSymbol = undefined;
+var currentValue = undefined;
+
+
+document.addEventListener("DOMContentLoaded", ()=>{
 	getDataFromBackground();
 });
 
 
+function createCryptosTabel(data){
+	let tbody = document.querySelector("#available-cryptos-table-body");
+	let fragment = new DocumentFragment();
+	for (let symbol in data){
+		let tr = document.createElement("tr");
+		// row number		
+		let row = document.createElement("th");
+		row.scope = "col";
+		tr.appendChild(row);
+		// symbol name
+		let sym = document.createElement("td");
+		sym.innerText = symbol;
+		tr.appendChild(sym);
+		// euro conversion
+		let eur = document.createElement("td");
+		eur.innerText = data[symbol].EUR;
+		tr.appendChild(eur);
+		// usd conversion
+		let usd = document.createElement("td");
+		usd.innerText = data[symbol].USD;
+		tr.appendChild(usd);
+		// chf conversion
+		let chf = document.createElement("td");
+		chf.innerText = data[symbol].CHF;
+		tr.appendChild(chf);
+		// apend to fragment
+		if (!allowedSymbols.includes(symbol)){
+			tr.style.color = "lightgrey";
+		}
+		fragment.appendChild(tr);
+	}
+	tbody.appendChild(fragment);
+}
+
+
+function setUpdateDate(date){
+	document.querySelector("#update-date").innerText = date.toLocaleString();
+}
+
+
 function handleResponse(response){
-	console.log(response);
-	document.querySelector("#test").innerText = response;
+	console.log("[INFO] received data from background: ", response);
+	createCryptosTabel(response.data);
+	setUpdateDate(response.date);
+	currentValue = parseFloat(response.data.MIOTA.EUR);
 }
 
 
@@ -22,3 +74,18 @@ function getDataFromBackground(){
 
 // Listener
 browser.runtime.onMessage.addListener(handleResponse);
+
+
+calcButton.addEventListener("click", (event)=>{
+	cryptOutput.value = "";								// clear crypto output
+	let m = fiatInput.value.match(/^\d+[.,]?\d*$/);		// check the input with regex
+	console.log(m, currentValue);
+	if (m !== null){									// convert and set the crypto output
+		cryptOutput.value = parseFloat(m[0].replace(",", ".")) / currentValue;
+	} else {											// clear everything
+		fiatInput.value = "";
+		cryptOutput.value = "";
+	}
+	event.preventDefault();
+});
+
